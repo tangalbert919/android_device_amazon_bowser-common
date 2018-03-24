3c2eba1886fb18c3342bbe601b785924b2bdd7cf
Albert Tang <tangalbert919@gmail.com>
2018-03-24 14:49:17 -0500

    Remove omap4-common dependency
    We need to rely on hardware/ti/omap4 for Android Oreo, not device/amazon/omap4-common.
    
    Change-Id: I6a7561d3b96af0fdd7e40f1a5a87e6ec6430ea7c
    Signed-off-by: Albert Tang <tangalbert919@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -15,17 +15,21 @@
 # limitations under the License.
 #
 
-BOWSER_COMMON_FOLDER := device/amazon/bowser-common
+COMMON_FOLDER := device/amazon/bowser-common
 
-# inherit from common
--include device/amazon/omap4-common/BoardConfigCommon.mk
+# inherit from omap4
+include hardware/ti/omap4/BoardConfigCommon.mk
 
 # Sensors
 BOARD_USES_GENERIC_INVENSENSE := false
 
+# Audio
+BOARD_USES_GENERIC_AUDIO := false
+
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(COMMON_FOLDER)/bluetooth
 
 # Camera
 TI_OMAP4_CAMERAHAL_VARIANT := true
@@ -40,8 +44,15 @@ USE_CAMERA_STUB := false
 -include vendor/amazon/bowser-common/BoardConfigVendor.mk
 
 # Kernel
+BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_IMAGE_NAME := zImage
+BOARD_CANT_BUILD_RECOVERY_FROM_BOOT_PATCH := true
+TARGET_KERNEL_CONFIG := android_omap4_defconfig
+TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
 TARGET_BOOTLOADER_BOARD_NAME := bowser
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
 
 # Connectivity - Wi-Fi
 BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
@@ -59,6 +70,15 @@ BOARD_CACHEIMAGE_PARTITION_SIZE := 681574400
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 929038336
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 13777223680
 BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := f2fs
+TARGET_USERIMAGES_USE_EXT4 := true
+ifeq ($(HOST_OS),linux)
+TARGET_USERIMAGES_USE_F2FS := true
+endif
+
+# vold
+BOARD_VOLD_MAX_PARTITIONS := 32
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 
 # Dolby enhancements
 OMAP_ENHANCEMENT_DOLBY_DDPDEC51_MULTICHANNEL := true
@@ -68,14 +88,26 @@ endif
 
 # Graphics
 BOARD_CREATE_AMAZON_HDCP_KEYS_SYMLINK := true
+BOARD_EGL_WORKAROUND_BUG_10194508 := true
+
+# Misc.
+BOARD_NEEDS_CUTILS_LOGS := true
+BOARD_HARDWARE_CLASS += $(OMAP4_NEXT_FOLDER)/cmhw
+BOARD_SEPOLICY_DIRS += \
+    $(OMAP4_NEXT_FOLDER)/sepolicy
 
 # Recovery
 TARGET_RECOVERY_PRE_COMMAND := "echo 0 > /sys/block/mmcblk0boot0/force_ro; echo -n 7 | dd of=/dev/block/mmcblk0boot0 bs=1 count=1 seek=4104 ; sync; \#"
+TARGET_RECOVERY_PIXEL_FORMAT := "BRGA_8888"
 BOARD_CANT_BUILD_RECOVERY_FROM_BOOT_PATCH := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_UMS_LUNFILE := "/sys/devices/virtual/android_usb?android0/f_mass_storage/lun/file"
 
 # TWRP Config
 TW_INTERNAL_STORAGE_PATH := "/data/media/0"
 TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
+TW_INCLUDE_FB2PNG := true
 BOARD_HAS_NO_REAL_SDCARD := true
 RECOVERY_SDCARD_ON_DATA := true
 TW_MAX_BRIGHTNESS := 254
@@ -85,7 +117,7 @@ TW_CUSTOM_BATTERY_PATH := /sys/class/power_supply/bq27541
 LEGACY_RIL := true
 
 # hack the ota
-TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := ./$(COMMON_FOLDER)/releasetools/bowser_ota_from_target_files.py
-TARGET_RELEASETOOL_MAKE_RECOVERY_PATCH_SCRIPT := ./$(COMMON_FOLDER)/releasetools/bowser_make_recovery_patch
+TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := $(COMMON_FOLDER)/releasetools/bowser_ota_from_target_files.py
+TARGET_RELEASETOOL_MAKE_RECOVERY_PATCH_SCRIPT := $(COMMON_FOLDER)/releasetools/bowser_make_recovery_patch
 
 TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
--- common.mk
+++ common.mk
@@ -1,9 +1,39 @@
-COMMON_FOLDER := device/amazon/omap4-common
-BOWSER_COMMON_FOLDER := device/amazon/bowser-common
+# Copyright (C) 2013 The CyanogenMod Project
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This file includes all definitions that apply to ALL Amazon Kindle Fire devices, and
+# are also specific to otter devices
+#
+# Everything in this directory will become public
+
+COMMON_FOLDER := device/amazon/bowser-common
 
 $(call inherit-product, $(COMMON_FOLDER)/common.mk)
+$(call inherit-product-if-exists, hardware/ti/omap4/omap4.mk)
 
-DEVICE_PACKAGE_OVERLAYS += $(BOWSER_COMMON_FOLDER)/overlay/aosp
+DEVICE_PACKAGE_OVERLAYS += $(COMMON_FOLDER)/overlay/aosp
+
+PRODUCT_CHARACTERISTICS := tablet,nosdcard
+
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+LOCAL_KERNEL := $(DEVICE_FOLDER)/kernel
+else
+LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
 
 # Hardware HALs
 PRODUCT_PACKAGES += \
@@ -35,32 +65,94 @@ PRODUCT_PACKAGES += \
     sensors.omap4 \
     libinvensense_hal \
     libmllite \
-    libmplmpu \
+    libmplmpu
+
+# Wifi
+PRODUCT_PACKAGES += \
+    dhcpcd.conf \
+    libwpa_client \
+    hostapd \
+    wpa_supplicant \
+    wpa_supplicant.conf
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+    make_ext4fs \
+    sdcard \
+    e2fsck \
+    mkfs.f2fs \
+    fsck.f2fs \
+    fibmap.f2fs
+
+# Audio Support
+PRODUCT_PACKAGES += \
+    libaudioutils \
+    Music \
+    tinyplay \
+    tinymix \
+    tinycap \
+    audio_policy.default \
+    audio.usb.default \
+    audio.r_submix.default
+
+# DRM
+PRODUCT_PACKAGES += \
+    libwvm \
+
+# Misc / Testing
+PRODUCT_PACKAGES += \
+    evtest \
+    strace \
+    libjni_pinyinime \
+    sh
 
 # Rootfs
 PRODUCT_COPY_FILES += \
-    $(BOWSER_COMMON_FOLDER)/init.bowser-common.rc:/root/init.bowser-common.rc
+    $(LOCAL_KERNEL):kernel \
+    $(COMMON_FOLDER)/default.prop:/root/default.prop \
+    $(COMMON_FOLDER)/init.bowser-common.rc:/root/init.bowser-common.rc
+
+# Codecs
+PRODUCT_COPY_FILES += \
+    $(COMMON_FOLDER)/prebuilt/etc/media_codecs.xml:/system/etc/media_codecs.xml \
+    $(COMMON_FOLDER)/prebuilt/etc/media_profiles.xml:/system/etc/media_profiles.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
     frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
-    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    $(call add-to-product-copy-files-if-exists,packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml)
 
 # Prebuilts
 PRODUCT_COPY_FILES += \
-    $(BOWSER_COMMON_FOLDER)/prebuilt/etc/audio_policy.conf:/system/etc/audio_policy.conf \
-    $(BOWSER_COMMON_FOLDER)/prebuilt/etc/mixer_paths.xml:/system/etc/mixer_paths.xml \
-    $(BOWSER_COMMON_FOLDER)/prebuilt/etc/smc_normal_world_android_cfg.ini:/system/etc/smc_normal_world_android_cfg.ini \
-    $(BOWSER_COMMON_FOLDER)/prebuilt/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
-    $(BOWSER_COMMON_FOLDER)/prebuilt/usr/idc/AtmelTouch.idc:system/usr/idc/AtmelTouch.idc \
-    $(BOWSER_COMMON_FOLDER)/prebuilt/usr/idc/cyttsp4-i2c.idc:system/usr/idc/cyttsp4-i2c.idc \
-    $(BOWSER_COMMON_FOLDER)/prebuilt/usr/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl\
-    $(BOWSER_COMMON_FOLDER)/prebuilt/vendor/etc/audio_effects.conf:/system/vendor/etc/audio_effects.conf
+    $(COMMON_FOLDER)/prebuilt/etc/audio_policy.conf:/system/vendor/etc/audio_policy.conf \
+    $(COMMON_FOLDER)/prebuilt/etc/mixer_paths.xml:/system/vendor/etc/mixer_paths.xml \
+    $(COMMON_FOLDER)/prebuilt/etc/smc_normal_world_android_cfg.ini:/system/vendor/etc/smc_normal_world_android_cfg.ini \
+    $(COMMON_FOLDER)/prebuilt/etc/wifi/wpa_supplicant.conf:system/vendor/etc/wifi/wpa_supplicant.conf \
+    $(COMMON_FOLDER)/prebuilt/usr/idc/AtmelTouch.idc:system/usr/idc/AtmelTouch.idc \
+    $(COMMON_FOLDER)/prebuilt/usr/idc/cyttsp4-i2c.idc:system/usr/idc/cyttsp4-i2c.idc \
+    $(COMMON_FOLDER)/prebuilt/usr/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl\
+    $(COMMON_FOLDER)/prebuilt/vendor/etc/audio_effects.conf:/system/vendor/etc/audio_effects.conf \
+    $(COMMON_FOLDER)/prebuilt/etc/gps.conf:/system/etc/gps.conf
 
 # Prebuilts /system/etc/bluetooth
 PRODUCT_COPY_FILES += \
-    $(BOWSER_COMMON_FOLDER)/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
+    $(COMMON_FOLDER)/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
 
 $(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
 $(call inherit-product-if-exists, vendor/amazon/bowser-common/bowser-common-vendor.mk)
